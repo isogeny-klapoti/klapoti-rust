695214429831f21853b5ed9b0e2b582e38cdf673
miha-stopar <stopar.miha@gmail.com>
2025-06-30 19:17:52 +0200

    params updated

--- src/lib.rs
+++ src/lib.rs
@@ -38,6 +38,33 @@ pub mod ec509 {
     crate::schemes::klapoti::define_klapoti! {}
 }
 
+pub mod ec64 {
+    use crate::util::Big;
+    pub type Fp = crate::fields::Fp64::Fp;
+    pub type Fq = crate::fields::Fp64Ext::Fp2;
+    crate::ec::eccore::define_ec_core! {}
+    crate::theta::theta::define_theta_structure! {}
+    crate::schemes::klapoti::define_klapoti! {}
+}
+
+pub mod ec128 {
+    use crate::util::Big;
+    pub type Fp = crate::fields::Fp128::Fp;
+    pub type Fq = crate::fields::Fp128Ext::Fp2;
+    crate::ec::eccore::define_ec_core! {}
+    crate::theta::theta::define_theta_structure! {}
+    crate::schemes::klapoti::define_klapoti! {}
+}
+
+pub mod ec256 {
+    use crate::util::Big;
+    pub type Fp = crate::fields::Fp256::Fp;
+    pub type Fq = crate::fields::Fp256Ext::Fp2;
+    crate::ec::eccore::define_ec_core! {}
+    crate::theta::theta::define_theta_structure! {}
+    crate::schemes::klapoti::define_klapoti! {}
+}
+
 pub mod ec512 {
     use crate::util::Big;
     pub type Fp = crate::fields::Fp512::Fp;
@@ -51,6 +78,33 @@ pub mod ec768 {
     use crate::util::Big;
     pub type Fp = crate::fields::Fp768::Fp;
     pub type Fq = crate::fields::Fp768Ext::Fp2;
+    crate::ec::eccore::define_ec_core! {}
+    crate::theta::theta::define_theta_structure! {}
+    crate::schemes::klapoti::define_klapoti! {}
+}
+
+pub mod ec1024 {
+    use crate::util::Big;
+    pub type Fp = crate::fields::Fp1024::Fp;
+    pub type Fq = crate::fields::Fp1024Ext::Fp2;
+    crate::ec::eccore::define_ec_core! {}
+    crate::theta::theta::define_theta_structure! {}
+    crate::schemes::klapoti::define_klapoti! {}
+}
+
+pub mod ec1536 {
+    use crate::util::Big;
+    pub type Fp = crate::fields::Fp1536::Fp;
+    pub type Fq = crate::fields::Fp1536Ext::Fp2;
+    crate::ec::eccore::define_ec_core! {}
+    crate::theta::theta::define_theta_structure! {}
+    crate::schemes::klapoti::define_klapoti! {}
+}
+
+pub mod ec2048 {
+    use crate::util::Big;
+    pub type Fp = crate::fields::Fp2048::Fp;
+    pub type Fq = crate::fields::Fp2048Ext::Fp2;
     crate::ec::eccore::define_ec_core! {}
     crate::theta::theta::define_theta_structure! {}
     crate::schemes::klapoti::define_klapoti! {}
